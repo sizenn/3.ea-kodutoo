750b26436f89b21e6ced8a6eb068e8a0ffda3265
Rasmus Kivipõld <sizenn@users.noreply.github.com>
2018-10-23 21:18:24 +0300

    big update
    1. added sendString, which sends input content every time input content changes.
    2. added sendEnter that makes contentscript click on the link
    3. updated contenyscript listeners
    4. added scrollIntoView to the link

--- popup.js
+++ popup.js
@@ -1,12 +1,30 @@
 let input
 
-function send () {
+function sendString () {
+    console.log("sending string")
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {"word": input.value})
       });
 };
 
+function sendEnter () {
+    console.log("sending enter")
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {"enter": "enter"})
+      });
+}
+
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("search").addEventListener("click", send);
     input = document.getElementById('input');
+    input.addEventListener("input", sendString)
+
+    input.addEventListener("keyup", function(event) {
+        if (event.keyCode === 13) {
+            sendEnter()
+            input.value = ''
+        }
+    });
+    
 });
+
+
--- search.js
+++ search.js
@@ -1,16 +1,29 @@
+let link
+
 function search (str) {
     console.log(str)
     for (const a of document.querySelectorAll("a")) {
         if (a.textContent.includes(str)) {
-          a.scrollIntoView()
-          break
+            link = a;
+            link.scrollIntoView()
+            break
         }
       }
   }
+function enter () {
+    console.log(link.textContent)
+    console.log("here")
+    link.click();
+}
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        if (request.word !== ""){
-            search(request.word)
+        if (request.word){
+            if (request.word !== ""){
+                search(request.word)
+            }
+        }
+        if(request.enter){
+            enter()
         }
 });
