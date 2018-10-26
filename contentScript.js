9bce9fe5ab72837f7d2b9a488fa4444e126c3090
Rasmus Kivipõld <sizenn@users.noreply.github.com>
2018-10-27 01:17:03 +0300

    linting

--- contentScript.js
+++ contentScript.js
@@ -2,72 +2,72 @@ let current
 let linkArray = []
 let pointer
 function search (str) {
-    //change colors back
-    pointer = 0
-    if(linkArray.length != 0){
-        linkArray.forEach(function(elem) {
-            elem.style.backgroundColor = ""
-        })
-    }
-    //empty linkArray
-    linkArray = []
-    //push new links
-    for (let a of document.querySelectorAll("a")) {
-        let lowerCase = a.textContent.toLowerCase()
-        if (lowerCase.includes(str.toLowerCase())) {
-            a.style.backgroundColor = "#FF99FF"
-            linkArray.push(a)
-        }
-    }
-    if(linkArray.length != 0){
-        newCurrent()
-    }else{
-        current = null;
+  // change colors back
+  pointer = 0
+  if (linkArray.length !== 0) {
+    linkArray.forEach(function (elem) {
+      elem.style.backgroundColor = ''
+    })
+  }
+  // empty linkArray
+  linkArray = []
+  // push new links
+  for (let a of document.querySelectorAll('a')) {
+    let lowerCase = a.textContent.toLowerCase()
+    if (lowerCase.includes(str.toLowerCase())) {
+      a.style.backgroundColor = '#FF99FF'
+      linkArray.push(a)
     }
   }
+  if (linkArray.length !== 0) {
+    newCurrent()
+  } else {
+    current = null
+  }
+}
 
-//set new active link and scroll to it
+// set new active link and scroll to it
 function newCurrent () {
-    current = linkArray[pointer];
-    current.style.backgroundColor = "#66FFFF"
-    current.scrollIntoView()
-    window.scrollBy(0, -150);
+  current = linkArray[pointer]
+  current.style.backgroundColor = '#66FFFF'
+  current.scrollIntoView()
+  window.scrollBy(0, -150)
 }
 
 function enter () {
-    if(current != null){
-        current.click()
-    }
+  if (current != null) {
+    current.click()
+  }
 }
 
 function next () {
-    if(pointer < linkArray.length-1){
-        pointer ++
-        current.style.backgroundColor = "#FF99FF"
-        newCurrent()
-    }
+  if (pointer < linkArray.length - 1) {
+    pointer++
+    current.style.backgroundColor = '#FF99FF'
+    newCurrent()
+  }
 }
 
 function previous () {
-    if(pointer > 0){
-        pointer --
-        current.style.backgroundColor = "#FF99FF"
-        newCurrent()
-    }
+  if (pointer > 0) {
+    pointer--
+    current.style.backgroundColor = '#FF99FF'
+    newCurrent()
+  }
 }
 
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        if (request.word){
-            search(request.word)
-        }
-        if (request.enter){
-            enter()
-        }
-        if (request.next) {
-            next()
-        }
-        if(request.previous){
-            previous()
-        }
-});
+  function (request, sender, sendResponse) {
+    if (request.word) {
+      search(request.word)
+    }
+    if (request.enter) {
+      enter()
+    }
+    if (request.next) {
+      next()
+    }
+    if (request.previous) {
+      previous()
+    }
+  })
--- popup.js
+++ popup.js
@@ -1,49 +1,46 @@
 let input
 
 function sendString () {
-    if (input.value.length >= 3){
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {"word": input.value})
-            });
-    }
+  if (input.value.length >= 3) {
+    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {'word': input.value})
+    })
+  }
 };
 
 function sendEnter () {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {"enter": "enter"})
-      });
+  chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {'enter': 'enter'})
+  })
 }
 
 function sendNext () {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {"next": "next"})
-      });
+  chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {'next': 'next'})
+  })
 }
 
 function sendPrevious () {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {"previous": "previous"})
-      });
+  chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {'previous': 'previous'})
+  })
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    input = document.getElementById('input');
-    input.addEventListener("input", sendString)
-    input.focus()
+document.addEventListener('DOMContentLoaded', function () {
+  input = document.getElementById('input')
+  input.addEventListener('input', sendString)
+  input.focus()
 
-    input.addEventListener("keydown", function(event) {
-        if (event.keyCode === 13) {
-            sendEnter()
-            input.value = ''
-        }
-        if (event.ctrlKey && event.keyCode == 78){
-            sendNext()
-        }
-        if (event.ctrlKey && event.keyCode == 66){
-            sendPrevious()
-        }
-    });
-    
-});
-
-
+  input.addEventListener('keydown', function (event) {
+    if (event.keyCode === 13) {
+      sendEnter()
+      input.value = ''
+    }
+    if (event.ctrlKey && event.keyCode === 78) {
+      sendNext()
+    }
+    if (event.ctrlKey && event.keyCode === 66) {
+      sendPrevious()
+    }
+  })
+})
